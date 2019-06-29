00201d5b2ccee6cb739283ac8cacef8e1331d70a
Leandro Collares <leco@uvic.ca>
2019-06-29 15:41:05 -0300

    Updates

---
+++ precache-manifest.f64a7de6302cc04fc976e3277ca1fd9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a23c5e4dff1d9d75c63",
+    "url": "/rainfall-in-canada/static/js/runtime~main.3a23c5e4.js"
+  },
+  {
+    "revision": "34e6388c02b91a4f6203",
+    "url": "/rainfall-in-canada/static/js/main.34e6388c.chunk.js"
+  },
+  {
+    "revision": "cf2937c01dcdc8b7cb33",
+    "url": "/rainfall-in-canada/static/js/1.cf2937c0.chunk.js"
+  },
+  {
+    "revision": "34e6388c02b91a4f6203",
+    "url": "/rainfall-in-canada/static/css/main.5dc089cf.chunk.css"
+  },
+  {
+    "revision": "4c01dcc3d54ba13b8e98c6980f91b82f",
+    "url": "/rainfall-in-canada/index.html"
+  }
+];
